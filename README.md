ebafd6f73604f9a85b056fcd571b583b15475bb1
Kiril Videlov <kiril.vi@runbox.no>
2018-07-14 21:08:39 +0200

    Bugfixes
    Checks if .ssh is already present. Also moves the zsh setup out of the
    main file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+Main setup
 `curl --silent https://raw.githubusercontent.com/krlvi/dotfiles/master/setup.sh | bash`
+
+Zsh setup
+`zsh`
+`git clone --recursive git@github.com:krlvi/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"`
+```
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+```
+`chsh -s /bin/zsh`
--- setup.sh
+++ setup.sh
@@ -6,8 +6,13 @@ HOSTS_FILE=https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 
 DOTFILES=~/dotfiles
 
-echo "Generating a new ssh key pair"
-ssh-keygen -t rsa -b 4096 -C "kiril.vi@runbox.no"
+echo "Updating the Hosts file";
+curl $HOSTS_FILE | sudo tee -a /etc/hosts > /dev/null;
+
+if test -e ! ~/.ssh/; then
+  echo "Generating a new ssh key pair"
+  ssh-keygen -t rsa -b 4096 -C "kiril.vi@runbox.no"
+fi
 
 echo "Installing xcode command line tools";
 xcode-select --install;
@@ -25,7 +30,7 @@ else
 fi
 
 echo "Installing ${DOTFILES}/Brewfile packages";
-brew bundle --file=$DOTFILES_REPO/Brewfile;
+brew bundle --file=$DOTFILES/Brewfile;
 
 echo "Installing Sdkman"
 curl -s "https://get.sdkman.io" | bash
@@ -37,9 +42,6 @@ echo "Setting up VIM youcompleteme"
 cd ~/.vim/plugged/YouCompleteMe;
 ./install.py --go-completer;
 cd -;
-
-echo "Updating the Hosts file";
-curl $HOSTS_FILE | sudo tee -a /etc/hosts > /dev/null;
 
 echo "Setting up symlinks"
 ln -s "$DOTFILES/vim" ~/.vim;
@@ -56,13 +58,3 @@ defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-echo "Setting up zprezto";
-zsh;
-git clone --recursive "$PREZTO_REMOTE" "${ZDOTDIR:-$HOME}/.zprezto";
-setopt EXTENDED_GLOB;
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}";
-done
-
-echo "Setting zsh as default"
-chsh -s $(which zsh)
