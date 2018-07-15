1afd6297e214ef09a5d111b91cc469e3be5c6abe
Kiril Videlov <kiril.vi@runbox.no>
2018-07-15 08:06:03 +0200

    Simplifying the setup script
    Moving part of the setup to the README file, simplifying setup.sh which
    only takes care of the brew packages

--- README.md
+++ README.md
@@ -1,13 +1,39 @@
-Main setup
+* Xcode command line tools
+`xcode-select --install`
+
+* Brew packages
 `curl --silent https://raw.githubusercontent.com/krlvi/dotfiles/master/setup.sh | bash`
 
-Zsh setup
-`zsh`
-`git clone --recursive git@github.com:krlvi/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"`
+* zsh
 ```
+zsh
+git clone --recursive git@github.com:krlvi/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+chsh -s /bin/zsh
 ```
-`chsh -s /bin/zsh`
+
+* ssh
+`ssh-keygen -t rsa -b 4096 -C "kiril.vi@runbox.no"`
+
+* SDKMan
+`curl -s "https://get.sdkman.io" | bash`
+
+* Hosts file
+`curl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | sudo tee -a /etc/hosts > /dev/null`
+
+* Spacemacs
+`git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d`
+
+* OSX
+```
+chflags nohidden ~/Library
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.dashboard mcx-disabled -bool true
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+```
--- setup.sh
+++ setup.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 DOTFILES_REMOTE=https://github.com/krlvi/dotfiles.git
-PREZTO_REMOTE=https://github.com/krlvi/prezto.git
-HOSTS_FILE=https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-
 DOTFILES=~/dotfiles
-
-echo "Updating the Hosts file";
-curl $HOSTS_FILE | sudo tee -a /etc/hosts > /dev/null;
-
-echo "Installing xcode command line tools";
-xcode-select --install;
 
 if test ! $(which brew); then
   echo "Installing homebrew";
@@ -27,29 +18,8 @@ fi
 echo "Installing ${DOTFILES}/Brewfile packages";
 brew bundle --file=$DOTFILES/Brewfile;
 
-echo "Installing Sdkman"
-curl -s "https://get.sdkman.io" | bash
-
-echo "Setting up Spacemacs";
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d;
-
-echo "Setting up VIM youcompleteme"
-cd ~/.vim/plugged/YouCompleteMe;
-./install.py --go-completer;
-cd -;
-
 echo "Setting up symlinks"
 ln -s "$DOTFILES/vim" ~/.vim;
 ln -s "$DOTFILES/vimrc" ~/.vimrc;
 ln -s "$DOTFILES/gitconfig" ~/.gitconfig;
 ln -s "$DOTFILES/tmux.conf" ~/.tmux.conf;
-
-echo "Configuring OSX"
-chflags nohidden ~/Library
-defaults write com.apple.finder ShowPathbar -bool true
-defaults write com.apple.finder ShowStatusBar -bool true
-defaults write com.apple.dashboard mcx-disabled -bool true
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-
